0e6931fb64eb6bd1203eefe20d0f7a19760fdd59
João Victor <143547570+victorvlone@users.noreply.github.com>
2023-11-24 11:14:02 -0300

    Add files via upload

---
+++ Escola/util/Alunos.java
@@ -0,0 +1,23 @@
+package Escola.util;
+
+public class Alunos {
+
+    private String nome;
+    private String sexo;
+    private String datadeNascimento;
+    private String cpf;
+    private String numerodeMatricula;
+
+    public Alunos() {
+    }
+
+    public Alunos(String nome, String sexo, String datadeNascimento, String cpf, String numerodeMatricula) {
+
+            this.nome = nome;
+            this.sexo = sexo;
+            this.datadeNascimento = datadeNascimento;
+            this.cpf = cpf;
+            this.numerodeMatricula = numerodeMatricula;
+
+    }
+}
---
+++ Escola/util/Professores.java
@@ -0,0 +1,19 @@
+package Escola.util;
+
+public class Professores {
+    
+    private String nome;
+    private String sexo;
+    private String datadeNascimento;
+    private String cpf;
+    private String numerodeInscricao;
+
+    public Professores(String nome, String sexo, String datadeNascimento, String cpf, String numerodeInscricao) {
+        this.nome = nome;
+        this.sexo = sexo;
+        this.datadeNascimento = datadeNascimento;
+        this.cpf = cpf;
+        this.numerodeInscricao = numerodeInscricao;
+    }
+    
+}
